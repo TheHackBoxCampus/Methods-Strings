273f6179b64d98d18366b0dfe2c4c7a62bac0359
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 20:10:13 -0500

    feat (TypeOfMethods): localeCompare()

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 let string = "Hello world!";
+let char = "a"
 
-const TypeOfMethods = (str,regex) => {
-    return str.search(regex); 
-    /*El método search() ejecuta una 
-    búsqueda de una coincidencia entre 
-    una expresión regular y este objeto String.*/
+const TypeOfMethods = (chr, con) => {
+    return chr.localeCompare(con); 
+    /*devuelve un número que indica si 
+    una cadena de referencia viene antes 
+    o después, o si es la misma que la 
+    cadena dada en el orden de clasificación..*/
 } 
 
-console.log(TypeOfMethods(string, /[^\w\s]/g)); 
+console.log(TypeOfMethods(char, "c")); 
 
 
 
