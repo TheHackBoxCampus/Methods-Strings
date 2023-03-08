b91533ccc0a9c60919c20ea1748a58082ce6c1e0
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 20:38:04 -0500

    feat (TypeOfMethods): split()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let string = "Hello world!";
 
 const TypeOfMethods = (str) => {
-    return str.substr(1,2); 
-    /* devuelve una parte de la cadena, 
-    comenzando en el índice especificado 
-    y extendiéndose por un número determinado 
-    de caracteres */
+    return str.split(" "); 
+    /* divide un objeto de tipo String 
+    en un array (vector) de cadenas 
+    mediante la separación de la cadena 
+    en subcadenas*/
 } 
 
 console.log(TypeOfMethods(string)); 
