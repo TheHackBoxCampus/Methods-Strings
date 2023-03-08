5ce444cb0228f93a164a766bfb3cc9287db8d622
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-08 13:50:11 -0500

    feat(aritmethicOperators): ✨ Marh.max()
    modifique el codigo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // operadores aritmeticos, OBJETO MATH
-const aritmethicOperators = () => Math.min(2,6,1,6,35,12,3);
-/*Retorna el numero menor de los parametros establecidos*/
+const aritmethicOperators = () => Math.max(2,6,1,6,35,12,3);
+/*Retorna el numero mayor de los parametros establecidos*/
 console.log(aritmethicOperators()); 
 
