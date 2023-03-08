118bd1ce6e050f822ea33ab85c1e50ca1a536103
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 19:22:15 -0500

    feat (TypeOfMethods): slice()

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let string = "Hello world!";
 
 const TypeOfMethods = (str) => {
-    return str.replace("H", "A") 
-    /*Remplaza el parametro especificado
-    OJO* remplaza la primera coincidencia*/
+    return str.slice(2,7) 
+    /*Extrae una sección de una cadena y 
+    devuelve una nueva cadena.*/
+
 } 
 
 console.log(TypeOfMethods(string)); 
