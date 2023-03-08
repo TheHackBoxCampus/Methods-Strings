e067dd659eb3b1d85d1ecb65a828231cfe6087a3
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 19:33:23 -0500

    feat (TypeOfMethods):  length()

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let string = "Hello world!";
 
 const TypeOfMethods = (str) => {
-    return str.repeat(2)
-    /*El método repeat() construye y devuelve 
-    una nueva cadena que contiene el número especificado
-    de copias de la cadena en la cual fue llamada, 
-    concatenados.*/
+    return str.length
+    /*En realidad lenght es una propiedad, y retorna
+    el numero de caracteres que tiene la cadena*/
 } 
 
 console.log(TypeOfMethods(string)); 
