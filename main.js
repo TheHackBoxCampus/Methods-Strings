2f07c98af5c95139bf91a708975908dfa80b3e1b
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 20:44:09 -0500

    feat (TypeOfMethods): trimEnd()

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let string = "   hello world!   ";
 
 const TypeOfMethods = (str) => {
-    return str.trimStart(); 
-    /*quita el espacio del principio */
+    return str.trimEnd(); 
+    /*quita el espacio del final */
 } 
 
 console.log(TypeOfMethods(string)); 
