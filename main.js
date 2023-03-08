44953fa894b0e2409b8ddfbc27b9fd8ce6de0b9f
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-08 13:48:52 -0500

    feat(aritmethicOperators): ✨ Marh.min()
    modifique el codigo

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // operadores aritmeticos, OBJETO MATH
-const aritmethicOperators = (num, exp) => Math.pow(num,exp);
-/*Retorna una potencia, el primero parametro es el numero que se va a 
-elevar al segundo parametro*/
-console.log(aritmethicOperators(16,2)); 
+const aritmethicOperators = () => Math.min(2,6,1,6,35,12,3);
+/*Retorna el numero menor de los parametros establecidos*/
+console.log(aritmethicOperators()); 
 
