994f643f52f593119e42af43f6281de38b3f92ce
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 19:52:04 -0500

    feat (TypeOfMethods): fromCharCode()

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let string = "Hello world!";
 
-const TypeOfMethods = (str) => {
-    return str.codePointAt(5)
-    /*devuelve un número entero no negativo
-     que es el valor del punto de código 
-     Unicode en la posición dada*/
+const TypeOfMethods = () => {
+    return String.fromCharCode(100)
+    /*devuelve una cadena creada a partir de 
+    la secuencia especificada de unidades de 
+    código UTF-16.*/
 } 
 
-console.log(TypeOfMethods(string)); 
+console.log(TypeOfMethods()); 
 
 
 
