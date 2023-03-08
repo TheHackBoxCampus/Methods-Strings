729dec087a2b79a81c1486ae8ea23bad9fb05f1b
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-08 13:44:31 -0500

    feat(aritmethicOperators): ✨  Marh.pow()
    modifique el codigo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // operadores aritmeticos, OBJETO MATH
-const aritmethicOperators = (num) => Math.sqrt(num);
-/*Retorna la raiz cuadrada del numero que se pasa como parametro*/
-console.log(aritmethicOperators(16)); 
+const aritmethicOperators = (num, exp) => Math.pow(num,exp);
+/*Retorna una potencia, el primero parametro es el numero que se va a 
+elevar al segundo parametro*/
+console.log(aritmethicOperators(16,2)); 
 
