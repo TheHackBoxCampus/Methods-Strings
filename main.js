2ff9567405d7c057dcde0a9206cf8ae468f2274f
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 19:29:15 -0500

    feat (TypeOfMethods): repeat()

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let string = "Hello world!";
 
 const TypeOfMethods = (str) => {
-    return str.substring(2,6) 
-    /*Devuelve un subconjunto de un objeto String.*/
+    return str.repeat(2)
+    /*El método repeat() construye y devuelve 
+    una nueva cadena que contiene el número especificado
+    de copias de la cadena en la cual fue llamada, 
+    concatenados.*/
 } 
 
 console.log(TypeOfMethods(string)); 
