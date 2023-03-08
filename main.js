98639f055961b2d5b232828e6777f83c973fe52e
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 20:06:49 -0500

    feat (TypeOfMethods): search()

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 let string = "Hello world!";
 
-const TypeOfMethods = (str) => {
-    return str.lastIndexOf("o"); 
-    /*dado un argumento: una subcadena 
-    para buscar, busca en toda la cadena 
-    de llamada y devuelve el índice de 
-    la última aparición de la subcadena 
-    especificada*/
+const TypeOfMethods = (str,regex) => {
+    return str.search(regex); 
+    /*El método search() ejecuta una 
+    búsqueda de una coincidencia entre 
+    una expresión regular y este objeto String.*/
 } 
 
-console.log(TypeOfMethods(string)); 
+console.log(TypeOfMethods(string, /[^\w\s]/g)); 
 
 
 
