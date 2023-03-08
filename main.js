af258285df786cf657bb031c98597cc88241b770
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-08 13:52:15 -0500

    feat(aritmethicOperators): ✨ Marh.PI
    modifique el codigo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // operadores aritmeticos, OBJETO MATH
-const aritmethicOperators = () => Math.max(2,6,1,6,35,12,3);
-/*Retorna el numero mayor de los parametros establecidos*/
+const aritmethicOperators = () => Math.PI;
+/*Retorna el numero PI, una constante*/
 console.log(aritmethicOperators()); 
 
