8ba2a2a47eac7cb258edc7b281f4b07e1badd019
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-07 19:04:39 -0500

    feat (TypeOfMethods): includes()

---
+++ main.js
@@ -0,0 +1,12 @@
+let string = "Hello world!";
+
+const TypeOfMethods = (str) => {
+    return str.includes("o"); 
+    /* Busca dentro del string y verifica si el 
+	 parametro existe dentro */
+} 
+
+console.log(TypeOfMethods(string)); 
+
+
+
