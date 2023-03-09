cb5986bb518112c772bbfe601b2ac60b19af2ea4
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-08 21:59:58 -0500

    feat(logicOperators): ✨ AND
    Modifique el código

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// operadores logico,  OR
-const logicOperators = (logic1, logic2) => logic1 || logic2;
-/*Si cualquiera de sus argumentos es true, retorna true, de 
-lo contrario retorna false..*/
+// operadores logico,  AND
+const logicOperators = (logic1, logic2) => logic1 && logic2;
+/* retorna true si ambos operandos son valores verdaderos y 
+false en cualquier otro caso.*/
 console.log(logicOperators(true, false)); 
 
