9916f355dd9c318e55ea134d618aeade5c92063c
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-09 07:12:51 -0500

    feat(aritmethicOperators):✨ Math.log()
    Modifique el codigo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Mas metodos del Objeto MATH
-const aritmethicOperators = (a,b) => Math.imul(a,b);
-/*Equivalente a a * b, pero a nivel de bits.*/
-console.log(aritmethicOperators(40, 0xffffffff));  // -40
+const aritmethicOperators = (x) => Math.log(x);
+/*Devuelve el logaritmo natural en base e de x.*/
+console.log(aritmethicOperators(0xffffffff));  
 
