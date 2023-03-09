5d8412ff8b833bf50464c623e675c60eca921a3e
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-09 06:54:59 -0500

    feat(aritmethicOperators): ✨ Math.fround()
    Modifique el codigo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Mas metodos del Objeto MATH
-const aritmethicOperators = (num) => Math.ceil(num);
-/*Devuelve x con redondeo superior (el entero más alto)*/
-console.log(aritmethicOperators(201.234312)); 
+const aritmethicOperators = (num) => Math.fround(num);
+/*Devuelve x con redondeo (flotante con precisión simple)*/
+console.log(aritmethicOperators(323.234312)); 
 
