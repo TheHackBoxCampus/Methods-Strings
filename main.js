9f5ef49bff1d610421889c2cbb15e1e4b699124e
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-08 21:56:14 -0500

    feat(logicOperators): ✨ OR
    Modifique el nombre de la función y su contenido

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// operadores aritmeticos, OBJETO MATH
-const aritmethicOperators = () => Math.PI;
-/*Retorna el numero PI, una constante*/
-console.log(aritmethicOperators()); 
+// operadores logico,  OR
+const logicOperators = (logic1, logic2) => logic1 || logic2;
+/*Si cualquiera de sus argumentos es true, retorna true, de 
+lo contrario retorna false..*/
+console.log(logicOperators(true, false)); 
 
