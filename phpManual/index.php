022e024748cad29aa87493b5c4291896f5241629
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 13:44:16 -0500

    feat: :sparkles: Operadores 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,10 +1,40 @@
 <?php
-   // tipos de datos 
+   // operadores aritmeticos
+    $addition = 1 + 1; 
+    $subtraction = 1 - 1;
+    $multiplication = 1 * 1;
+    $division = 5 / 1;  
+    $exponentiation = 5**2;
+    
 
-   $text = "cadenas de texto";
-   $numbers = 13;
-   $floats = 13.3; 
-   $booleans = true; 
-   $arrays = [12,3,4];
-   $asociativeArrays = array ("1" => 1, "3" => 3);
+   // operadores de asignacion 
+    $equal = "=";
+
+    // operadores de comparacion 
+    $union = "+"; 
+    $equality = "==";
+    $idetity = "===";
+    $inequality = "!="; 
+    $options = "<>";
+    $noIdentity = "!=="; 
+    $greaterThan = ">";
+    $lessThan = "<"; 
+    $greaterThanOrEqual = ">=";
+    $lessThanOrEqual = "<="; 
+    $spaceshipOperator = "<=>"; 
+
+    // operadores logicos 
+    $and = "&&";
+    $or = "||";
+    $not = "!"; 
+    
+
+    // operadores de incremento 
+    $incrementing = "++"; 
+    $decrementing = "--"; 
+    $preIncrement = "++var";
+    $postIncrement = "var++";
+    $preDecrent = "--var";
+    $postDecrent = "var--";
+    
 ?>
