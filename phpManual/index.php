c23b8969282a3ed6fe4b18b8a3fbb0d8c5736b88
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 14:20:33 -0500

    feat: :sparkles: Estructuras repetitivas 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,39 +1,59 @@
 <?php
-   // TODO: Estructuras condicionales
+   // TODO: Estructuras repetitivas
+   
    /*
-    * Las estructuras condicionales son una estructura de control de flujo en programación que permite que 
-    * el programa tome decisiones basadas en si se cumple o no una condición
+      * Las estructuras repetitivas, también conocidas como estructuras de control de bucle, son 
+      * herramientas fundamentales en la programación que permiten repetir la ejecución de un bloque de 
+      * código varias veces.
    */
 
-    // TODO: IF ElSE
-
-    // * Aqui las estructuras manejan los operadores logicos - comparacion - bits 
-
-    $number = 10; 
-
-    if($number < 10) {
-        echo $number." es menor que 10"; 
-    }else {
-        echo $number." es mayor o igual a 10";
-    }
-
-    // TODO: SWITCH 
-
-    /*
-        * En PHP, el "switch" es una estructura de control que permite ejecutar diferentes bloques de código 
-        * dependiendo del valor de una variable
+   // TODO: Bucle While
+   
+    /* 
+      * permite repetir la ejecución de un bloque de código mientras se cumpla una 
+      * condición.
     */
 
-    switch($number) {
-        case 9:
-            echo $number." es menor que 10";  
-            break; 
-        case 10: 
-            echo $number." es mayor o igual a 10";
-            break;
-        default: 
-            echo "ninguna de las anteriores"; 
-    }
-    
+   $number = 1; 
 
+   while($number <= 10) {
+        echo $number."\n"; 
+        $number++;
+   }
+
+    // TODO: Bucle Do While
+
+    /*
+      * similar al bucle while, pero garantiza que el bloque de código se ejecuta al 
+      * menos una vez, independientemente de si se cumple o no la condición
+    */
+   $number = 1; 
+   echo "\n"; 
+
+    do {
+        echo $number."\n";
+        $number++;
+    }while($number <= 10); 
+
+    // TODO: Bucle For
+
+    /* 
+      * Permite repetir la ejecucion de un bloque de codigo un numero fijo de veces,
+      * controlando el numero de iteraciones mediante un contador 
+    */
+    echo "\n";
+    for($newNumber = 1; $newNumber <= 10; $newNumber++) {
+        echo $newNumber."\n"; 
+    } 
+
+    // TODO: For Each
+
+    // * Ciclo repetitivo para arrays que contengan clave-valor o contenido. 
+
+    $peoples = ["enrique", "raul", "marquez"]; 
+    echo "\n";
+
+    foreach($peoples as $people) {
+        echo $people."\n"; 
+    }
 ?>
