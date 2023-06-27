2c2b75813753d65606e0799f34e2440526be4364
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 13:56:09 -0500

    feat: :sparkles: Arreglos y algunos metodos 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,40 +1,30 @@
 <?php
-   // operadores aritmeticos
-    $addition = 1 + 1; 
-    $subtraction = 1 - 1;
-    $multiplication = 1 * 1;
-    $division = 5 / 1;  
-    $exponentiation = 5**2;
+    // ejemplo de array asociativo
+    $arr = [ 
+        "name" => "miller",
+        "age" => 18,
+        "status" => "Good",
+        "school" => [
+            "name" => "campus",
+            "address" => "Zona franca"
+            ]
+        ];
+        
+
+    // algunas funciones de arreglos
+    $arr2 = ["celular", "ip", "manzana"]; 
     
-
-   // operadores de asignacion 
-    $equal = "=";
-
-    // operadores de comparacion 
-    $union = "+"; 
-    $equality = "==";
-    $idetity = "===";
-    $inequality = "!="; 
-    $options = "<>";
-    $noIdentity = "!=="; 
-    $greaterThan = ">";
-    $lessThan = "<"; 
-    $greaterThanOrEqual = ">=";
-    $lessThanOrEqual = "<="; 
-    $spaceshipOperator = "<=>"; 
-
-    // operadores logicos 
-    $and = "&&";
-    $or = "||";
-    $not = "!"; 
+    // in_array - Buscar elementos de un arreglo
+    var_dump(in_array("celular", $arr2));   
     
+    // ordenar elementos de un arreglo
+    $numbers = [1,2,3,4,5,6,7,9];
+    sort($numbers); // de menor a mayor
+    rsort($numbers); // de mayor a menor 
 
-    // operadores de incremento 
-    $incrementing = "++"; 
-    $decrementing = "--"; 
-    $preIncrement = "++var";
-    $postIncrement = "var++";
-    $preDecrent = "--var";
-    $postDecrent = "var--";
-    
+   asort($arr); // Ordena por valores (orden alfabetico)
+   arsort($arr); // Ordena por valor (Z primero)
+   ksort($arr); // Ordena por llaves (orden alfabetico)
+   krsort($arr); // Ordena por llaves (Z primero) 
+   
 ?>
