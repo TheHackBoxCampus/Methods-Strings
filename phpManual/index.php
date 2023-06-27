0274982a99cbfed2d4dd2250e7dc6c9384b93913
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 14:07:05 -0500

    feat: :sparkles: Estructuras condicionales 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,5 +1,39 @@
 <?php
-    $arr = ["hola", "mundo"];
-    echo isset($arr); // comprueba si una variable o un elemento de un array esta definido y no el null
-    echo empty($arr); // comprueba si una variable o un elemento de un array está vacío
+   // TODO: Estructuras condicionales
+   /*
+    * Las estructuras condicionales son una estructura de control de flujo en programación que permite que 
+    * el programa tome decisiones basadas en si se cumple o no una condición
+   */
+
+    // TODO: IF ElSE
+
+    // * Aqui las estructuras manejan los operadores logicos - comparacion - bits 
+
+    $number = 10; 
+
+    if($number < 10) {
+        echo $number." es menor que 10"; 
+    }else {
+        echo $number." es mayor o igual a 10";
+    }
+
+    // TODO: SWITCH 
+
+    /*
+        * En PHP, el "switch" es una estructura de control que permite ejecutar diferentes bloques de código 
+        * dependiendo del valor de una variable
+    */
+
+    switch($number) {
+        case 9:
+            echo $number." es menor que 10";  
+            break; 
+        case 10: 
+            echo $number." es mayor o igual a 10";
+            break;
+        default: 
+            echo "ninguna de las anteriores"; 
+    }
+    
+
 ?>
