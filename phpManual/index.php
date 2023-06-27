3313a68bed7e765129a63e8c1ddc0d99317f177c
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 15:33:21 -0500

    feat: :sparkles: Clases  y metodos estáticos 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -38,7 +38,17 @@
 */
 
 class Animals {
+    /*
+        TODO: Clases
+        ? Concepto
+        * Como vemos en el apartado introductorio a la programación orientada a objetos una clase es una 
+        * plantilla que nos permite definir las características y comportamientos de los objetos que se pueden 
+        * crear a partir de dicha clase a continuación veremos un ejercicio en el cual se evidencia como crear 
+        * una clase y como instancia de una clase en PHP.
+    */
+
     private $fundation; // TODO: Propiedad
+
     /*
         TODO: Modificadores de acceso 
 
@@ -66,6 +76,37 @@ class Animals {
     public function __construct(string $name, string $age) {
         $this->name = $name;
         $this->age = $age;
+    }
+
+    public static function  greet(string $greet): ?string {
+        /* 
+            TODO: Metodos estáticos
+            ? Concepto
+             * Un método estático es un método que pertenece a la clase en sí y no a una 
+             * instancia específica de la clase. A diferencia de los métodos de instancia, los métodos estáticos se 
+             * pueden llamar directamente en la clase sin necesidad de crear un objeto o instancia de la misma.
+             
+            ? Caracteristicas
+            *  No requieren una instancia: Los métodos estáticos se pueden invocar directamente desde la 
+            * clase, utilizando la sintaxis Clase::metodoEstatico(), sin necesidad de crear un objeto de la 
+            * clase.
+            
+            *  No pueden acceder a propiedades de instancia: Los métodos estáticos no pueden acceder 
+            * directamente a las propiedades de instancia de la clase, ya que no tienen una instancia 
+            * específica asociada. Solo pueden acceder a propiedades estáticas (variables estáticas) que 
+            * pertenezcan a la clase.
+            
+            *  No pueden utilizar $this: En un método estático, no se puede utilizar la palabra clave $this
+            * para hacer referencia a la instancia actual de la clase, ya que no hay una instancia asociada.
+            
+            *  Útiles para utilidades compartidas: Los métodos estáticos son útiles para definir funciones o 
+            * utilidades que no dependen del estado de una instancia específica. Se pueden utilizar para 
+            * operaciones globales, cálculos matemáticos, acceso a bases de datos, manipulación de 
+            * archivos, etc
+            
+        */
+        
+        return $greet; 
     }
 
     function getName() { // TODO: Metodo
