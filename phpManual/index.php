c50fb3cd5aa608238b83643eda905f4f54a27358
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 14:30:16 -0500

    feat:  ✨ funciones definidas por el usuario 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,59 +1,26 @@
 <?php
-   // TODO: Estructuras repetitivas
-   
-   /*
-      * Las estructuras repetitivas, también conocidas como estructuras de control de bucle, son 
-      * herramientas fundamentales en la programación que permiten repetir la ejecución de un bloque de 
-      * código varias veces.
-   */
-
-   // TODO: Bucle While
-   
-    /* 
-      * permite repetir la ejecución de un bloque de código mientras se cumpla una 
-      * condición.
-    */
-
-   $number = 1; 
-
-   while($number <= 10) {
-        echo $number."\n"; 
-        $number++;
-   }
-
-    // TODO: Bucle Do While
+    // TODO: Funciones definidas por el usuario 
 
     /*
-      * similar al bucle while, pero garantiza que el bloque de código se ejecuta al 
-      * menos una vez, independientemente de si se cumple o no la condición
+        * En programación, las funciones definidas por el usuario son bloques de código que se pueden llamar 
+        * y ejecutar en cualquier parte de un programa para realizar una tarea específica
+    
     */
-   $number = 1; 
-   echo "\n"; 
 
-    do {
-        echo $number."\n";
-        $number++;
-    }while($number <= 10); 
+    declare(strict_types=1); // * Esta declaración se utiliza para activar la verificación estricta de tipos en un archivo PHP.
 
-    // TODO: Bucle For
-
-    /* 
-      * Permite repetir la ejecucion de un bloque de codigo un numero fijo de veces,
-      * controlando el numero de iteraciones mediante un contador 
-    */
-    echo "\n";
-    for($newNumber = 1; $newNumber <= 10; $newNumber++) {
-        echo $newNumber."\n"; 
-    } 
-
-    // TODO: For Each
-
-    // * Ciclo repetitivo para arrays que contengan clave-valor o contenido. 
-
-    $peoples = ["enrique", "raul", "marquez"]; 
-    echo "\n";
-
-    foreach($peoples as $people) {
-        echo $people."\n"; 
+    function greet(string $greetName):string {
+        return $greetName."\n";
     }
+
+    /*
+        * funciones con valor de retorno estricto
+    */
+
+    function sum($num, $num2):int {
+        return $num + $num2; 
+    }
+
+     echo greet("Hola mundo");
+     echo sum(3,4);  
 ?>
