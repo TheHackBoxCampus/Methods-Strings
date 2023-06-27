64a0df54737a17e053aab133cab8a9d5bf07b240
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 16:15:37 -0500

    feat: :sparkles: Composer 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,35 +1,22 @@
 <?php
-   // TODO: Namespace
+   // TODO: Composer
    // ? Concepto 
    /* 
-    * En PHP, la palabra clave use se utiliza en la definición de espacios de nombres (namespace) para 
-    * importar clases, funciones y constantes desde otros espacios de nombres.
-    
-    * Cuando se utiliza use en la definición de un espacio de nombres, se está especificando una ruta corta 
-    * (alias) para acceder a un elemento específico de otro espacio de nombres. Por ejemplo, si se tiene 
-    * una clase MiClase definida en el espacio de nombres MiEspacioDeNombres y se quiere utilizar esa 
-    * clase en otro espacio de nombres
-   
- */
+        * Composer es un administrador de dependencias para PHP que permite instalar librerías de terceros 
+        * para facilitar el desarrollo con php. Componser es el equivalente a npm para node JS.
+   */
 
-    namespace App; 
+    // ? Comandos Basicos
 
-    class Animals {
-        public static function getInfo() {
-            echo "info"; 
-        } 
-    }
-
-     /*
-        TODO: Desde otro fichero .PHP
-
-        ! Puedes importar la clase animals con el abreviado
-
-        * use App\Animals; 
-
-        ! utilizar metodos y propiedades que tenga desde otro fichero
-        ! Siempre teniendo en cuenta que el autoload tiene que esta funcional.
-     */
-
+    /*
+        * Composer init -> Inicializa un proyecto de composer 
+        * Composer update -> actualiza el estado y dependencias de composer
+        
+        * Composer i -> instala las dependencias, por ejemplo si clonas un repositorio 
+        * y tiene dependencias puedes instalar esas dependencias con este comando
+  
+        * Composer require dependencia -> Instala dependencias
+        * Composer remove dependencia -> Desistala dependencias
+    */
 
 ?>
