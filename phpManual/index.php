f99d31c05021fbbc430e9686b92e1d1f4525e605
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 15:21:30 -0500

    feat: :sparkles:  POO. Programacion orientada a objetos 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,26 +1,55 @@
 <?php
-   // TODO: funciones mas utilizadas en la manipulacion de data
-    $arr = [
-        "name" => "roberto",
-        "age" => 21,
-        "address" => "Bucaramanga"
-    ];
+   // TODO: POO. Programacion orientada a objetos 
+   // ? Concepto 
+   /* 
+       * La programación orientada a objetos (POO) es un paradigma de programación que se basa en el 
+       * concepto de "objetos". Los objetos son entidades que representan conceptos del mundo real y que 
+       * pueden tener propiedades (atributos) y realizar acciones (métodos).
+   */
 
-    array_flip($arr); // * Intercambia las claves con sus valores correspondientes en un array
-    array_fill($arr); // * Rellena un array con un valor especificado. 
-    array_filter($arr); // * Filtra los elementos de un array utilizando una función de devolución de llamada.
-    array_map($arr); // * Aplica una función a cada elemento de un array y devuelve un nuevo array con los resultados. 
-    array_reduce($arr); // * Reduce un array a un solo valor aplicando una función de devolución de llamada
-    array_key_exists($arr); // * Comprueba si una clave existe en un array
-    in_array($arr); // * verifica si tiene valores el array  
-    array_rand($arr); //*  llaves aleatorias
-    array_unique($arr); // * muestra los valores uniques e ignora a los duplicados
-    array_intersect($arr, /* arr adicional */ $arr); // *  Devuelve un array con los valores comunes a todos los arrays dados
-    array_diff($arr, /* arr adicional */ $arr); // * Devuelve un array con los valores del primer array que no están presentes en los arrays adicionales
-    array_push($arr, "new value"); // * Agrega uno o mas elemento al final del array
-    array_revert($arr); // * revierte los valores de un array
-    array_shift($arr); // * elimina el primer valor de un array
-    array_unshift($arr, "new values"); // * agrega un valor al principio de un array
-    array_pop($arr); // * elmina el ultimo valor de un array
-    array_sum($arr); // * Devuelve la suma de un array numerico
+    // Conceptos importantes sobre POO
+
+    /*
+    *  Clase: Es una plantilla o definición que describe las características y comportamientos de los 
+    * objetos que se pueden crear a partir de ella.
+    
+    *  Objeto: Es una instancia de una clase. Representa un individuo o entidad específica y tiene 
+    * sus propias propiedades y comportamientos.
+    
+    *  Atributos: Son las propiedades o características de un objeto. Definen el estado de un objeto 
+    * y se representan mediante variables en la clase.
+    
+    *  Métodos: Son las acciones o comportamientos que un objeto puede realizar. Representan las 
+    * operaciones que pueden realizarse con un objeto y se definen como funciones en la clase.
+    
+    *  Encapsulación: Es el principio que establece que los atributos y métodos relacionados deben 
+    * agruparse en una clase para ocultar los detalles internos y exponer solo una interfaz pública. 
+    * Esto se logra mediante la especificación de niveles de acceso (público, privado, protegido) 
+    * para los atributos y métodos.
+    
+    *  Herencia: Es un mecanismo que permite crear nuevas clases basadas en clases existentes. La 
+    * clase que se utiliza como base se denomina "clase padre" o "superclase", y la clase que se 
+    * deriva se llama "clase hija" o "subclase". La herencia permite la reutilización de código y la 
+    * creación de jerarquías de clases.
+    
+    *  Polimorfismo: Es la capacidad de un objeto de tomar diferentes formas o comportarse de 
+    * diferentes maneras según el contexto. Permite utilizar una interfaz común para objetos de 
+    * diferentes clases y proporciona flexibilidad y extensibilidad en el diseño de programas.
+*/
+
+class Animals {
+    private $fundation; // TODO: Propiedad
+    
+    public function __construct(string $name, string $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function getName() { // TODO: Metodo
+        return $this->name; 
+    }    
+}
+
+$inst = new Animals("zeus", 12); // TODO: Objeto -> Instancia
+
 ?>
