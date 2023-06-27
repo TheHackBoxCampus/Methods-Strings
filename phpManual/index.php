39ed63cd9c3bf516a8af56b4ef79f0cff86adaba
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-06-27 14:36:52 -0500

    feat: :sparkles:  Include, require, include_once, require_once 🐘

--- phpManual/index.php
+++ phpManual/index.php
@@ -1,26 +1,19 @@
 <?php
-    // TODO: Funciones definidas por el usuario 
-
-    /*
-        * En programación, las funciones definidas por el usuario son bloques de código que se pueden llamar 
-        * y ejecutar en cualquier parte de un programa para realizar una tarea específica
+    // TODO: Include, require, include_once, require_once
     
-    */
-
-    declare(strict_types=1); // * Esta declaración se utiliza para activar la verificación estricta de tipos en un archivo PHP.
-
-    function greet(string $greetName):string {
-        return $greetName."\n";
-    }
-
     /*
-        * funciones con valor de retorno estricto
+        * "include y include_once": permiten incluir un archivo PHP en el programa. La diferencia entre ellas es 
+        * que include puede incluir el mismo archivo varias veces mientras que include_once asegura que el 
+        * archivo solo se incluya una vez en el programa.
+
+        * "require y require_once": son similares a include y include_once, pero en caso de que el archivo no 
+        * pueda ser encontrado, se detendrá la ejecución del programa. Al utilizar require_once, se asegura que 
+        * el archivo solo se incluya una vez en el programa.
+
+        - Algunos enlaces de referencia 
+         include: http://www.php.net/manual/es/function.include.php
+         include_once: http://www.php.net/manual/es/function.include-once.php
+         require: http://www.php.net/manual/es/function.require.php y
+         require_once: http://www.php.net/manual/es/function.require-once.php
     */
-
-    function sum($num, $num2):int {
-        return $num + $num2; 
-    }
-
-     echo greet("Hola mundo");
-     echo sum(3,4);  
 ?>
