24b9d99975fa3ceb5e85f5ec63d200ea64103ef3
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-15 12:21:20 -0500

    feat: ::boom:  Express

--- node/app.js
+++ node/app.js
@@ -1,27 +1,57 @@
 /*
-    TODO: http Request (hypertext transfer protocol)
+    TODO: Express 
     ? Concepto
-    * En Node.js, el objeto request (solicitud) es un objeto proporcionado por el módulo 
-    * http o https que representa la solicitud HTTP recibida por el servidor. Proporciona 
-    * información sobre la solicitud realizada por el cliente, como la URL, los 
-    * encabezados, los parámetros de consulta y el cuerpo de la solicitud.
-
+    * Express es un popular framework de aplicaciones web para Node.js. Proporciona 
+    * una capa adicional de abstracción sobre el módulo http incorporado de Node.js, lo 
+    * que facilita la creación de aplicaciones web y APIs de manera más eficiente y con 
+    * menos código.
 */
 /* 
-    ? Metodos de http Request
+    ? Caracteristica de express
+    * Enrutamiento: Express proporciona un enrutador flexible que permite definir rutas 
+    * y manejar las solicitudes HTTP correspondientes. Esto permite establecer la lógica 
+    * de controladores para diferentes rutas y métodos HTTP.
+ 
+    * Middleware: Express utiliza un modelo de middleware, donde se pueden agregar 
+    * funciones intermediarias (middleware) a la cadena de procesamiento de 
+    * solicitudes. Esto permite realizar acciones como autenticación, compresión, 
+    * registro y manipulación de solicitudes y respuestas antes de que lleguen a su 
+    * controlador final.
+
+    * Gestión de vistas: Express facilita el renderizado de vistas HTML utilizando 
+    * cualquier motor de plantillas (como EJS, Pug o Handlebars) para generar 
+    * contenido dinámico y enviarlo al cliente.
+
+    * Integración con servicios y recursos externos: Express proporciona una forma 
+    * sencilla de interactuar con servicios y recursos externos, como bases de datos, 
+    * servicios de almacenamiento en la nube y APIs de terceros, a través de bibliotecas 
+    * y módulos adicionales.
+
+    * Amplia comunidad y ecosistema: Express es uno de los frameworks más 
+    * populares y ampliamente adoptados en la comunidad de Node.js. Como resultado, 
+    * cuenta con una gran cantidad de recursos, módulos y complementos 
+    * desarrollados por la comunidad que extienden aún más su funcionalidad.
 */
 
-import { createServer } from "http";
+/*
+    ? inciar un servidor con Express
+*/
 
-let server = createServer((req, res) => {
-    /*
-        * req.url: La URL de la solicitud.
-        * req.method: El método HTTP utilizado en la solicitud (GET, POST, etc.).
-        * req.headers: Un objeto que contiene los encabezados de la solicitud.
-        * req.params: Los parámetros de ruta si estás utilizando un enrutador.
-        * req.query: Los parámetros de consulta si están presentes en la URL.
-        * req.body: El cuerpo de la solicitud (datos enviados en una solicitud POST o PUT)
-    */
+import express from "express"; 
+// var
+let app = express(); 
+
+// middleware
+app.use(express.json())
+
+// request 
+app.use("/example", (req, res) => {
+    res.status(200).send({status: 200, message: "hello world!"})
 })
+
+// listener
+app.listen(300, () => console.log("listening"))
+
+
 
 
