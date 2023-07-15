dde57c65f6a92bb6bd3abfdd52f7594a52085b3f
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-14 21:33:22 -0500

    feat:  💥  Modulos  🌐

--- node/app.js
+++ node/app.js
@@ -1,29 +1,38 @@
 /*
-    TODO: Process 
+    TODO: Modulos 
     ? Concepto
-    * En Node.js, el objeto process es un objeto global que proporciona información y 
-    * control sobre el proceso actual en ejecución. Es una instancia de la clase 
-    * EventEmitter, lo que significa que puede emitir y escuchar eventos
+    * El sistema de módulos de Node.js es un componente fundamental de la 
+    * plataforma que permite la modularidad y reutilización de código en aplicaciones y 
+    * proyectos desarrollados con Node.js
+*/
+/* 
+    ? Common JS 
+    * CommonJS es una especificación de módulos para JavaScript que define un 
+    * formato estándar para la organización y carga de módulos en entornos de 
+    * ejecución, como Node.js. Fue diseñado con el objetivo de proporcionar una 
+    * solución para modularizar el código JavaScript y facilitar la reutilización de 
+    * módulos en diferentes proyectos.
+
+    ? Formas de aplicacion con Common JS
+    - Exportar
+    * module.exports {...x || expresion ? true : false}
+    - Importar
+    * require(...data.ext) | data.__proto__.prop; 
+    
+    ? importar json con Common js   
+    * Utilizando los objetos en javascript podemos distribuir la data en:
+    * - object.prop || object[prop]
+
+    ? EcmaScript
+    * Los módulos nos permiten dividir nuestro código base en varios archivos para una mayor 
+    * facilidad de mantenimiento y esto nos permite evitar poner todo nuestro código en un archivo grande. 
+    * Para aprovechar | esta funcionalidad
+ 
+    ? Formas de aplicacion con Ecma Script
+    - Importar
+    * import { Module } from library 
+    - Exportar
+    * export { Function, Var, Const, boolean, etc} || export default value
 */
 
-process.exit() // * terminar el proceso
-process.env // * variables de entorno | sistema
 
-process.stdin 
-process.stdout  
-process.stderr 
-
-/*   
-* Estos objetos permiten la interacción con la entrada estándar (stdin), 
-* la salida estándar (stdout) y la salida 
-* de error estándar (stderr) respectivamente
-*/
-
-process.on(() => {
-     return true
-})
-/*  
-    * Un método para registrar devoluciones de llamada 
-    * para manejar eventos del proceso. Puedes utilizar este método para capturar 
-    * eventos como la salida del proceso
-*/
