3858aef10669b80018b71952d058699c55e2a682
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-15 12:40:17 -0500

    feat: :boom: Modulo dotenv

--- node/app.js
+++ node/app.js
@@ -1,47 +1,61 @@
 /*
-    TODO: Express Router()
+    TODO: Dotenv()
     ? Concepto
-    * En Express, un router es una forma de organizar y gestionar las rutas de una 
-    * aplicación web de manera modular. Los routers permiten agrupar rutas 
-    * relacionadas y sus respectivos controladores en un lugar específico.
-    * Un router en Express es un objeto que proporciona métodos para definir rutas y 
-    * gestionar las solicitudes HTTP asociadas a esas rutas. Puedes utilizar varios 
-    * routers en una aplicación Express para dividir y organizar las rutas en diferentes 
-    * módulos o archivos.
+    * Las variables de entorno en Node.js son variables específicas del entorno en el 
+    * que se ejecuta una aplicación. Son valores que se pueden configurar 
+    * externamente al código y se utilizan para almacenar información sensible, 
+    * configuraciones personalizadas u otros datos relevantes para la aplicación.
+    * En Node.js, las variables de entorno se pueden acceder utilizando el objeto 
+    * process.env. Este objeto proporciona acceso a todas las variables de entorno 
+    * definidas en el sistema operativo donde se está ejecutando la aplicación.
 */
 
 /*
-    ? inciar un servidor con Express
-*/
-// TODO: Archivo app.js
-import express from "express"; 
-import router from "./router.js";
+    ? Caracteristicas
 
-// var
-let app = express(); 
-
-// middleware
-app.use(express.json())
-
-// request 
-app.use("/example", router);
-
-// listener
-app.listen(300, () => console.log("listening"))
-
-/*
-    TODO: Archivo router.js
-
-    ? import { Router } from "express";
-
-    ? let router = Router(); 
+    * Configuración de la aplicación: Puedes utilizar variables de entorno para 
+    * almacenar valores de configuración, como credenciales de bases de datos, claves 
+    * de API, URL de servicios externos, etc. Esto permite mantener esta información 
+    * sensible separada del código fuente y facilita la configuración de la aplicación en 
+    * diferentes entornos (desarrollo, producción, pruebas, etc.).
     
-    ? router.get("/ruta1", (req, res) => {
-    ?     res.send("hello world")
-    ? })
-
-    ? export default router; 
+    * Parámetros personalizables: Puedes utilizar variables de entorno para permitir la 
+    * personalización de ciertos comportamientos de la aplicación. Por ejemplo, podrías 
+    * tener una variable de entorno que determine el número de elementos a mostrar 
+    * por página en una paginación, o el idioma predeterminado a utilizar.
+    
+    * Depuración y registro: Las variables de entorno también pueden ser útiles para 
+    * habilitar o deshabilitar la depuración o el registro de la aplicación en función de la 
+    * configuración del entorno. Esto permite tener un mayor control sobre los mensajes 
+    * de registro o habilitar características adicionales de depuración en entornos de 
+    * desarrollo o pruebas
 */
+
+/*
+    ? uso
+    * Instalas la libreria con npm 
+    * creas el archivo .env y defines las variables
+    * importas los paquetes
+    * cargas las variables en el archivo con el metodo @config()
+    * utilizas las variables definidas en el archivo .env
+*/
+
+// TODO: archivo app.js
+// * importar
+import dotenv from "dotenv"
+// * cargar
+dotenv.config();
+// * utlizar
+let vars = process.env.config(); 
+
+
+// TODO: archivo .env
+/*
+    config= {...properties}
+*/
+
+
+
 
 
 
